0e44a4aafdfd04b1d8ea5989043579b025ec77dc
yusufu66 <yusufuw2001@gmail.com>
2025-09-29 01:32:28 -0500

    just changes

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -2,15 +2,10 @@
 
 import React from "react";
 import BuyerDashboard from "@/components/BuyerDashboard";
-import DashboardNavigation from "../DashboardNavigation";
-import DashboardPreview from "../DashboatdPreview";
-// import './globals.css';
-
 
 // If you have a user provider/context, replace the demoUser below with the actual user from context.
 const demoUser = { id: 1, name: "Demo Buyer", email: "buyer@demo.com", role: "buyer" };
 
 export default function BuyerPage() {
-  return <DashboardPreview />;
-
+  return <BuyerDashboard user={demoUser} />;
 }
